1db17db601566b817fa3db2d547a2bfb4dcbb265
omaxx101 <oumersatmaxgmail.com>
2026-09-16 14:34:37 -0400

    finish

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,17 +41,17 @@ android {
         versionName = flutter.versionName
     }
 
-    signingConfigs {
+   signingConfigs {
     create("release") {
-        keyAlias = keystoreProperties["keyAlias"] as String
-        keyPassword = keystoreProperties["keyPassword"] as String
-        storeFile = file(keystoreProperties["storeFile"] as String)
-        storePassword = keystoreProperties["storePassword"] as String
+        keyAlias = keystoreProperties["keyAlias"].toString()
+        keyPassword = keystoreProperties["keyPassword"].toString()
+        storeFile = rootProject.file("app/${keystoreProperties["storeFile"]}")
+        storePassword = keystoreProperties["storePassword"].toString()
     }
 }
 
 buildTypes {
-    release {
+    getByName("release") {
         signingConfig = signingConfigs.getByName("release")
     }
 }
